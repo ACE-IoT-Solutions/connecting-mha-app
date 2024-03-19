c867303ffd499b657c2fffced4493c092ee7c22f
Vicki Wong <vicki.wong85@gmail.com>
2024-03-19 13:12:07 -0700

    add error modal

--- components/DismissableSwipeResponder.tsx
+++ components/DismissableSwipeResponder.tsx
@@ -1,7 +1,8 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useState } from "react";
+import { Modal, View, Text, StyleSheet } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { useWelcomeDismissed } from "../hooks/useAppStateData";
+import { Button } from "../components/Button";
 import {
   PanGestureHandler,
   State,
@@ -13,10 +14,15 @@ import {
 import Animated, { withTiming, useAnimatedStyle, useSharedValue } from "react-native-reanimated";
 import { MainNavigationProps } from "../types";
 import { theme } from "../theme";
+import { getApiEndpoint } from "../utils/api";
 
 export function DismissableSwipeResponder({ children }: { children: React.ReactNode }) {
   const { welcomeDismissedState, error, loading, getWelcomeDismissedState, setWelcomeDismissedEffect } =
     useWelcomeDismissed();
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [modalText, setModalText] = useState<string>(
+    "Could not connect to home gateway, please check your WiFi Connection and try again"
+  );
   const navigation = useNavigation<MainNavigationProps>();
   const initialPosition = useSharedValue(0);
   const position = useSharedValue(0);
@@ -33,6 +39,13 @@ export function DismissableSwipeResponder({ children }: { children: React.ReactN
 
   const onGestureStateChange = async ({ nativeEvent }: PanGestureHandlerStateChangeEvent) => {
     const { state, translationY, y } = nativeEvent;
+    const timeout = 4000;
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    const timeoutId = setTimeout(() => {
+      controller.abort();
+    }, timeout);
 
     if (state === State.BEGAN) {
       initialPosition.value = y;
@@ -40,7 +53,31 @@ export function DismissableSwipeResponder({ children }: { children: React.ReactN
 
     if (state === State.END) {
       if (translationY < -200) {
-        navigation.navigate("MacAddress");
+        try {
+          const url = await getApiEndpoint();
+          const endpoint = "/swaggesdfr.jsonsdfd";
+          const response = await fetch(url + endpoint, {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              Accept: "application/json",
+            },
+            signal,
+          });
+          clearTimeout(timeoutId);
+
+          if (response.status === 200) {
+            navigation.navigate("MacAddress");
+          } else {
+            setModalText(
+              "The Connecting MHA App requires local network access to communicate with Home Energy Management System, you can provide permission by going to Privacy and Security your device settings, or remove and reinstall the Connecting MHA App"
+            );
+            setIsModalVisible(true);
+          }
+        } catch (error) {
+          console.log({ error });
+          setIsModalVisible(true);
+        }
         await setWelcomeDismissedEffect();
         position.value = 0;
       } else {
@@ -49,13 +86,55 @@ export function DismissableSwipeResponder({ children }: { children: React.ReactN
     }
   };
 
+  function toggleModal() {
+    setIsModalVisible(!isModalVisible);
+  }
+
+  function ErrorModal() {
+    return (
+      <Modal transparent={true} visible={isModalVisible}>
+        <View style={styles.errorModal}>
+          <View style={styles.errorContainer}>
+            <Text style={styles.errorMessage}>{modalText}</Text>
+            <Button style={styles.errorButton} label="Try Again" onPress={toggleModal} />
+          </View>
+        </View>
+      </Modal>
+    );
+  }
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <PanGestureHandler onGestureEvent={onGesture} onHandlerStateChange={onGestureStateChange}>
         <View style={{ flex: 1, backgroundColor: theme.background }}>
           <Animated.View style={[{ flex: 1 }, animatedStyle]}>{children}</Animated.View>
+          <ErrorModal />
         </View>
       </PanGestureHandler>
     </GestureHandlerRootView>
   );
 }
+
+const styles = StyleSheet.create({
+  errorModal: {
+    flex: 1,
+    backgroundColor: "#000000aa",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  errorContainer: {
+    padding: 50,
+    borderRadius: 20,
+    backgroundColor: "#FFFFFF",
+    alignItems: "center",
+  },
+  errorMessage: {
+    fontSize: 18,
+  },
+  errorButton: {
+    marginTop: 20,
+    width: 180,
+    borderRadius: 50,
+  },
+});
